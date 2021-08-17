05af9d9b14d147904959e7970ec15b072984102f
Sopegina Elizaveta <54837560+0moemura0@users.noreply.github.com>
2021-08-17 22:16:43 +0300

    Feature/onboarding (#1)
    * add splash
    
    * add onboarding layout
    
    * refactor
    
    * update naming
    
    Co-authored-by: Sergey Kiselyov <48965524+kamikadze328@users.noreply.github.com>

---
+++ app/src/main/java/com/example/tinkoffproject/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.example.tinkoffproject
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+}
