8bf337d5553b517a4beddac8e45283f83c7d530f
0moemura0 <sopeginaliza@gmail.com>
2021-08-17 15:04:19 +0300

    add splash

---
+++ app/src/main/java/com/example/tinkoffproject/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.tinkoffproject
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+    private fun scheduleSplashScreen() {
+        val splashScreenDuration = getSplashScreenDuration()
+        Handler().postDelayed(
+            {
+
+            },
+            splashScreenDuration
+        )
+    }
+
+    private fun getSplashScreenDuration() = 2000L
+
+}
