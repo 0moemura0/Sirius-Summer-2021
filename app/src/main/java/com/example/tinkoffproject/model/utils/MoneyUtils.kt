82cfb61b1f7189e56628720305b77ab670151fcb
Sergey <48965524+kamikadze328@users.noreply.github.com>
2021-08-21 15:51:39 +0300

    make code more cleared and understandably

--- app/src/main/java/com/example/tinkoffproject/model/utils/MoneyUtils.kt
+++ app/src/main/java/com/example/tinkoffproject/model/utils/MoneyUtils.kt
@@ -20,14 +20,14 @@ fun formatMoney(amount: Int, currency: Currency): String {
 
 private const val SPACE = ' '
 fun formatMoney(amount: Int): String {
-    val putSpaceEveryNSymbols = 2
+    val putSpaceEveryNSymbols = 3
     val builder = StringBuilder()
     builder.append(amount)
-    var numberCount = 0
+    var numberCount = 1
     for (i in builder.length - 1 downTo 0) {
         if (numberCount == putSpaceEveryNSymbols) {
             builder.insert(i, SPACE)
-            numberCount = 0
+            numberCount = 1
         } else numberCount++
     }
     return builder.toString()
